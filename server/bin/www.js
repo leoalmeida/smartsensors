28e1f6a39a3e47bd5f5d17ed52cea4dcfca68c09
Leonardo Almeida <leoalmeida.rj@gmail.com>
2016-10-11 10:48:11 -0300

    fix www.js

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,11 +1,11 @@
 'use strict'
 
-//const http = require('http');
-//const port = process.env.PORT || 3000;
+const http = require('http');
+const port = process.env.PORT || 3000;
 const app = require('../app');
-/*const httpServer = http.createServer(app);
-const sockets = require('../sensors')(httpServer);
+const httpServer = http.createServer(app);
+//const sockets = require('../sensors')(httpServer);
 
 httpServer.listen(port, function(){
   console.log('Listen on port ' + port);
-});*/
+});
