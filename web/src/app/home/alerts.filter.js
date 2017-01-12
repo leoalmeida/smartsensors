e337ac65c01a94a2eabfbdcee6888e1223f9205f
Leonardo Almeida <leoalmeida.rj@gmail.com>
2017-01-12 11:47:10 -0200

    fix para corrigir a lista de alrtas que aparecem na Home.

--- web/src/app/home/alerts.filter.js
+++ web/src/app/home/alerts.filter.js
@@ -28,16 +28,16 @@
         return function (arr, list) {
             var i, j,  l = arr.length, r = [];
             for (i = 0; i < l; i += 1) {
-                for (j = 0; j < arr[i].length; j += 1) {
+                /*for (j = 0; j < arr[i].length; j += 1) {
                     var findValue = list.values.some(elem => ((elem.type == list.type) && (arr[i][elem.id]!=null)));
                     if ((findValue && !list.reversal) || (!findValue && list.reversal))
                         r.push(arr[i][j]);
+                }*/
+                if (list.values[i].type == list.type) {
+                     var findValue = arr.some(elem => (elem[list.values[i].id] != null));
+                     if ((findValue && !list.reversal) || (!findValue && list.reversal))
+                        r.push(list.values[i]);
                 }
-                /*if (list.values[i].type == list.type) {
-                 var findValue = arr.some(elem => (elem[list.values[i].id] != null));
-                 if ((findValue && !list.reversal) || (!findValue && list.reversal))
-                 r.push(list.values[i]);
-                 }*/
             }
             return r;
         };
