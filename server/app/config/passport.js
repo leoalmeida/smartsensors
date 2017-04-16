7f577260cb25845a35c5ea87a9c86de087df4548
Leonardo Almeida Silva Ferreira <leoalmeida.rj@gmail.com>
2017-04-16 20:51:39 -0300

    Update passport.js

--- server/app/config/passport.js
+++ server/app/config/passport.js
@@ -23,7 +23,7 @@ module.exports = function(passport){
 
 	//used to deserialize the user
 	passport.deserializeUser(function(id, done) {
-		User.findOne({ _id: id }, function (err, user) {
+		User.findById(id, function (err, user) {
 			done(err, user)
 		})
 	});
