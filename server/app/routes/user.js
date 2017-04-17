30bd8c7972c0f73b319f1d5db1d0c407c72f6cab
Leonardo Almeida Silva Ferreira <leoalmeida.rj@gmail.com>
2017-04-16 22:34:37 -0300

    Update user.js

--- server/app/routes/user.js
+++ server/app/routes/user.js
@@ -5,11 +5,12 @@ const router = require('express').Router();
 module.exports = function(app){
   const ctrl = app.controllers.userController;
 
-  router.get('/apis/user', ctrl.getAll);
-  router.get('/apis/user/:id', ctrl.getById);
-  router.post('/apis/user', ctrl.create);
-  router.put('/apis/user/:id', ctrl.update);
-  router.delete('/apis/user/:id', ctrl.remove);
+  router.post('/login', ctrl.login);
+  router.get('/auth/user', ctrl.getAll);
+  router.get('/auth/user/:id', ctrl.getById);
+  router.post('/auth/user', ctrl.create);
+  router.put('/auth/user/:id', ctrl.update);
+  router.delete('/auth/user/:id', ctrl.remove);
 
   return router;
 };
