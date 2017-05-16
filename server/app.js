c244d248b70faf2f2e278dcd532de23925797dd6
Leonardo Almeida silva ferreira <leoalmeida.rj@gmail.com>
2017-05-16 15:33:44 -0300

    fixes para o código heroku

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 const path = require('path');
 
 ////////////////////////////////////////////////////////////////
